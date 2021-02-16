468c5d4db61ade768848e5e3872d3de40e68cb1d
Munna Hossain <mayinuddin.munna@gmail.com>
2021-02-17 00:09:12 +0600

    barrrr

--- Web Content Edit fun.js
+++ Web Content Edit fun.js
@@ -1,1 +1,5 @@
+document.body.contentEditable = true;
+
+/*
 Firstly you go to any website . Then mouse right click to inspect , Copy or wright there to this Source code: "document.body.contentEditable = true;"then enter.
+*/
